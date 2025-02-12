76d40b712917303ee44dc10ec91211fc27118bd7
Mostafa Qobadi <106018598+MosQobadi@users.noreply.github.com>
2025-02-12 16:55:11 +0330

    add useReducer to useFetchMovies

--- src/hooks/useFetchMovies.js
+++ src/hooks/useFetchMovies.js
@@ -1,9 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+
+const initialState = {
+  movies: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataFetched":
+      return { ...state, movies: action.payload };
+
+    default:
+      break;
+  }
+}
 
 export function useFetchMovies(query, callback) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state);
 
   useEffect(() => {
     callback?.();
@@ -19,6 +38,8 @@ export function useFetchMovies(query, callback) {
         if (!res.ok) throw new Error("Something went wrong!");
 
         const data = await res.json();
+
+        dispatch({ type: "dataFetched", payload: data });
 
         setErrorMessage("");
         if (data.Response === "False") throw new Error("Movie not found");
